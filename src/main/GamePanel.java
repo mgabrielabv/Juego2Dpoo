be01fa73bc41c90519b3057cc0674f07610c6bc2
mgabrielabv <maria.32520312@uru.edu>
2025-12-10 20:20:39 -0400

    fix: creacion del paqueten entidad para colocar los jugadores y los monstruos

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,13 +1,14 @@
 package main;
 import javax.swing.JPanel;
 import java.awt.*;
+import entity.Player;
 
 public class GamePanel extends JPanel implements Runnable  {
      //SCreen SETTINGS
         final int originalTileSize=16; //16x16 tile
         final int scale=3;
 
-        final int tileSize=originalTileSize*scale; //48x48 tile
+        public final int tileSize=originalTileSize*scale; //48x48 tile
         final int maxScreenCol=16;
         final int maxScreenRow=12;
         final int screenwidth=tileSize*maxScreenCol; //768 pixels
@@ -15,6 +16,8 @@ public class GamePanel extends JPanel implements Runnable  {
         int FPS=60;
     KeyHandler keyH=new KeyHandler();
     Thread gameThread;
+    Player player=new Player(this,keyH);
+
     //set players default position
     int playerX=100;
     int playerY=100;
@@ -37,7 +40,7 @@ public class GamePanel extends JPanel implements Runnable  {
     public void run() {
         double drawInterval = 1000000000 / FPS;
         double nextDrawTime = System.nanoTime() + drawInterval;
-
+        
         while (gameThread != null) {
 
             Update();
@@ -63,29 +66,14 @@ public class GamePanel extends JPanel implements Runnable  {
         }
     }
         public void Update(){
-            if (keyH.upPressed ==true)
-            {
-                playerY= playerY-playerSpeed;
-            }
-            else if (keyH.downPressed ==true)
-            {
-                playerY= playerY+playerSpeed;
-            }
-            else if (keyH.leftPressed ==true)
-            {
-                playerX= playerX-playerSpeed;
-            }
-            else if (keyH.rightPressed ==true)
-            {
-                playerX= playerX+playerSpeed;
-            }
-        }
+        player.update();
+        };
+
+
         public void paintComponent(Graphics g){
         super.paintComponent(g);
-
         Graphics2D g2=(Graphics2D)g;
-        g2.setColor(Color.white);
-        g2.fillRect(playerX,playerY, tileSize, tileSize);
+        player.draw(g2);
         g2.dispose();
 
     }
